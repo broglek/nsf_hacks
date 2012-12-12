391b5a973ad03cce2939b5c35db3270160292b17
Sean Ma <ssma@stanford.edu>
2012-12-11 16:42:29 -0800

    have primitive validation working

--- runML.py
+++ runML.py
@@ -3,10 +3,21 @@
 
 from read_Matrix import ExampleData
 from scilearn import Learner
+from sklearn import cross_validation
 
 data = ExampleData(2000, 30799)
 data.readDataVectorFile('docwords_master_2000')
 data.readClassificationFile('ids_and_amounts_2000')
 
-learner = Learner(data)
-learner.test("NB")
+for n in range(5):
+  X_train, X_test, y_train, y_test = cross_validation.train_test_split(data.Xdata, data.Ydata, test_size=0.25, random_state=n, dtype=int)
+  
+  data_train = ExampleData(1500, 30799)
+  
+  data_train.Xdata = X_train
+  data_train.Ydata = y_train
+  
+  learner = Learner(data_train)
+  learner.learn("NB")
+  score = learner.test("NB", X_test, y_test)
+  print score
