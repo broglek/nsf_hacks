b87601ef46c3efeb9a498c147da70fe0a694a478
Kyle Brogle <broglek@bu.edu>
2012-12-11 16:51:38 -0800

    Added Stemming

--- generate_stemmed_docs.py
+++ generate_stemmed_docs.py
@@ -7,7 +7,7 @@ import re
 
 input1 = open("words_stemmed.txt")
 input2 = open("docwords_master_reindex")
-#output1 = open("words_stemmed.txt", 'w')
+output1 = open("docwords_master_stemmed", 'w')
 
 #numbering dict is lookup old number, get stemmed word.
 #stem_dict is lookup word, get new number.
@@ -20,7 +20,19 @@ index = 1
 for line in input1:
     x1, x2 = line.strip().split()
     numbering_dict[x1] = x2
-    if(!x2 in stem_dict):
+    if(not x2 in stem_dict):
         stem_dict[x2] = index
         index += 1
     
+for line in input2:
+    x1, x2, x3 = line.strip().split()
+    stemmed_word = numbering_dict[x2]
+    new_index = stem_dict[stemmed_word]
+    if (x1,new_index) in docs_dict:
+        docs_dict[(x1,new_index)] += x3
+    else:
+        docs_dict[(x1,new_index)] = x3
+
+
+for key in docs_dict:
+    output1.write(str(key[0]) + " " + str(key[1]) + " " + str(docs_dict[key]) + '\n')
